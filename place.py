333d8e1ece7cd681e31df23390cd2dbd1dc3d0e0
Abigail Pipkin <abigailpipkinttu@gmail.com>
2024-03-24 15:06:20 -0500

    adding parts of login page and retrieval

--- place.py
+++ place.py
@@ -3,3 +3,10 @@ import tkinter as tk
 # main window
 root = tk.Tk()
 root.title("Log-in Page")
+
+def login():
+    username = usernameEntry.get()
+    password = passwordEntry.get()
+    # retrieve values
+    print("Username:", username)
+    print("Password:", password)
