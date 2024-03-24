73e1a758af6a2145387816e2f2df3da9f54ca70b
Abigail Pipkin <abigailpipkinttu@gmail.com>
2024-03-24 14:28:17 -0500

    added parts of signup and proof of entry

--- grid.py
+++ grid.py
@@ -3,3 +3,13 @@ import tkinter as tk
 # main window
 root = tk.Tk()
 root.title("Sign-Up Page")
+
+def signup():
+    name = nameEntry.get()
+    email = emailEntry.get()
+    password = passwordEntry.get()
+
+# retrieved values
+    print("Name:", name)
+    print("Email:", email)
+    print("Password:", password)
